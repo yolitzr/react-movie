0e7cf3f73e7b32e0ca17f71af4af47108a297e97
Yolit Zacarias <yolit.zr@gmail.com>
2021-07-14 15:56:33 -0300

    Created custom hook  HomeFetch

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,53 +1,17 @@
 import React, { useState, useEffect } from 'react';
 //Config
 // import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config';
-//API
-import API from '../API.js'
-// Components
 
-//Hooks
-
+//Custmon Hooks
+import { useHomeFetch } from '../hooks/useHomeFetch';
 //Images
 // import NoCover from '../assets/img/no_image.jpg ';
 // import { ConsoleWriter } from 'istanbul-lib-report';
 
 export function Home() {
-	//useState
-	const [movieData, setmovieData] = useState();
-	const [loading, setLoading] = useState(false);
-	const [error, setError] = useState(false);
+ const { movieData, loading, error } = useHomeFetch();
 
-	const fetchMovies = async (page, searchTerm = '') => {
-		try {
-			setError(false);
-			setLoading(true);
-
-			const movies = await API.fetchMovies(searchTerm, page);
-
-            setmovieData((prevState) => ({
-				...movies,
-				results:
-					page > 1
-						? [...prevState.results, ...movies.results]
-						: [...movies.results],
-			}));
-		} catch (error) {
-			setError(true);
-		}
-
-        setLoading(false);
-	};
-
-	//useEffect - Initial render
-    useEffect(() => {
-        fetchMovies(1)
-    }, [])
-
-    console.log(movieData);
-    console.log(loading);
-    console.log(error);
-
-
+    console.log(movieData)
 
 	return <div>Home Page</div>;
 }
---
+++ src/hooks/useHomeFetch.js
@@ -0,0 +1,47 @@
+import { useState, useEffect, useRef } from "react";
+//API
+import API from '../API.js';
+
+const initialState = {
+    page: 0,
+    results: [],
+    total_pages: 0,
+    total_results: 0,
+};
+
+export function useHomeFetch() {
+    //useRef
+
+	//useState
+	const [movieData, setmovieData] = useState(initialState);
+	const [loading, setLoading] = useState(false);
+	const [error, setError] = useState(false);
+
+	const fetchMovies = async (page, searchTerm = '') => {
+		try {
+			setError(false);
+			setLoading(true);
+
+			const movies = await API.fetchMovies(searchTerm, page);
+
+			setmovieData((prevState) => ({
+				...movies,
+				results:
+					page > 1
+						? [...prevState.results, ...movies.results]
+						: [...movies.results],
+			}));
+		} catch (error) {
+			setError(true);
+		}
+
+		setLoading(false);
+	};
+
+	//useEffect - Initial render
+	useEffect(() => {
+		fetchMovies(1);
+	}, []);
+
+    return { movieData, loading, error };
+};
