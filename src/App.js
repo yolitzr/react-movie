1b5866ae9b540755cbdc0a685d4589461005ecdd
Yolit Zacarias <yolit.zr@gmail.com>
2021-07-18 16:45:30 -0300

    Created Routers Movie and NotFound

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import React from 'react';
+//Routing
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 //Styles
 import { GlobalStyle } from './GlobalStyle'
 //Components
 import { Header } from './components/Header'
 import { Home } from './components/Home';
+import { Movie } from './components/Movie'
+import { NotFound } from './components/NotFound';
 
 function App() {
     return (
-        <div className="App">
-            <Header />
-            <Home />
-            <GlobalStyle />
-        </div>
-    );
+		<Router>
+			<Header />
+			<Routes>
+				<Route path="/" element={<Home />} />
+                <Route path="/:movieId" element={<Movie />} />
+                <Route path="/*" element={<NotFound />}/>
+			</Routes>
+			<GlobalStyle />
+		</Router>
+	);
 }
 
 export default App;
